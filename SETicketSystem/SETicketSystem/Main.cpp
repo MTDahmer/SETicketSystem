ba62add763d10f459089a5ae54b27aafe3017e40
MTDahmer <41553823+MTDahmer@users.noreply.github.com>
2023-04-18 19:31:25 -0500

    Initial Upload
    Established project folder, created repo and uploaded initial draft of ticket.h

---
+++ SETicketSystem/SETicketSystem/Main.cpp
@@ -0,0 +1,12 @@
+/*Main CPP files, this will be where the code responsible for
+calling the other classes and running the program will be, all classes
+made for this application should be written as headers and refrenced here
+when needed. This way all classes will exist as seperate objects*/
+
+#include <iostream>
+#include <string>
+#include "ticket.h"
+
+int main() {
+
+}
---
+++ SETicketSystem/SETicketSystem/ticket.h
@@ -0,0 +1,88 @@
+#pragma once
+#ifndef ticket
+#define ticket
+#include <string>
+#include <vector>
+#include <iostream>
+class ticket {
+private:
+	int ticketID;
+	std::string customer;
+	std::string dateCreated;
+	std::string repairedItem;
+
+
+public:
+	std::string status;
+	std::string description;
+	std::vector <std::string> subticketList;
+	std::string technician;
+	std::vector < std::string> workentryList;
+	std::vector < std::string> charges;
+
+};
+
+//+callSubticketCreation() for calling the subticket creation process, leaving as psuedo until subticket is made
+//void callSubticketCreation(ticketID) {
+//	string newSubTicket = subticketCreation(ticketID);
+//	subticketList.append(newSubticket)
+//}
+//+ callUpdateCustomer() for calling update customer function, leaving as psuedo until subticket is made
+//Void callUpdateCustomer(customer) {
+//	updateCustomer(customer)
+//}
+//+ callAppendWork() for calling append work function, leaving as psuedo until subticket is made
+//Void callAppendWork(ticketID) {
+//	appendWork(ticketID)
+//}
+//+ changeDescription()
+std::string changeDescription(std::string description) {
+	std::cout << "Edit current description or append statement? \n 1. Append \n 2. Edit \n";
+	int choice;
+	std::string newDesc;
+	std::cin >> choice;
+	bool loop = true;
+	while (loop) {
+		if (choice == 1) {
+			std::cout << "Please enter the statement to append to the current description \n";
+			std::cin >> newDesc;
+			description = description + newDesc;
+			return description;
+		}
+		else if (choice == 2) {
+			std::cout << description;
+			std::cout << "Please enter a new description";
+			std::cin >> newDesc;
+			return newDesc;
+		}
+		else {
+			std::cout << "Invalid Selection. Please only enter the numbers listed as options";
+		}
+	}
+}
+//+ changeTech()
+std::string changeTech(std::string tech) {
+	std::string newTech;
+	std::cout << "Enter the name of new techician being assigned to the ticket";
+	std::cin >> newTech;
+	/*Following segment waiting for others first
+	Check if tech exists
+	If tech doesn't exist  {
+	Display “Invalid Tech”
+	Repeat prompt*/
+	return newTech;
+}
+//+ changeStatus() update the status of the ticket
+std::string changeStatus() {
+	std::string newStatus;
+	std::cout << "Please enter the new status";
+	std::cin >> newStatus;
+	return newStatus;
+}
+//+ callAddCharge()
+chargeObject callAddCharge(ticketID) {
+	addCharge(ticketID)
+}
+
+
+#endif

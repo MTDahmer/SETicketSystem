cb8cf4770993ab15cd07eefc85dc2829f67fe501
MTDahmer <mitchelltdahmer@gmail.com>
2023-04-30 15:07:09 -0500

    Incorporated all currently written files into main solution
    Troubleshot and patched subticket process to be in line with current approach.

---
+++ SETicketSystem/SETicketSystem/subticket.cpp
@@ -0,0 +1,61 @@
+#pragma once
+#ifndef subticket
+#define subticket
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <ctime>
+
+using namespace std;
+
+void createSubticket() {
+	int ticketid;
+	int subticketid;
+	int datecreated;
+	string additionalrepaired, status;
+	cout << "Enter the Main Ticket ID: \n";
+	cin >> ticketid;
+	cout << "Enter the Sub Ticket ID: \n";
+	cin >> subticketid;
+	time_t now = (0);
+	datecreated = now;
+	status = "Currently being worked on";
+	cout << "Enter additional work needed: \n";
+	cin >> additionalrepaired;
+	ofstream newSubTicket;
+	string ticketLocation = "C:\\SubTickets\\" + to_string(subticketid);
+	ticketLocation = ticketLocation + ".txt";
+
+	newSubTicket.open(ticketLocation);
+	newSubTicket << "Main Ticket ID: " << ticketid << "\n";
+	newSubTicket << "SubTicketID: " << subticketid << "\n";
+	newSubTicket << "Date Created: " << now << "\n";
+	newSubTicket << "Status" << status << "\n";
+	newSubTicket << "Additional work needed: " << additionalrepaired << "\n";
+	newSubTicket.close();
+}
+
+
+string subticketValueGrab(int subticketid, int line) {
+		ifstream requestedFile;
+		string fileName = "C:\\SubTickets\\" + to_string(subticketid);
+		fileName = fileName + ".txt";
+		requestedFile.open(fileName);
+		string value;
+		int count = 0;
+		while (getline(requestedFile, value)) {
+			if (count == line) {
+				value = value.substr(value.find(":") + 1);
+				requestedFile.close();
+				return value;
+			}
+			count++;
+		}
+
+
+
+	};
+
+
+
+#endif // !
